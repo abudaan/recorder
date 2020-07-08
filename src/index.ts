ad43f5ec7c8e2ba06dfeed2caf763944b05521c8
abudaan <daniel@abumarkub.net>
2020-07-08 19:41:00 +0200

    added hardware mix checkbox

--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,99 @@
 import "./styles/index.scss";
 
 const init = async (): Promise<void> => {
+  const video = document.getElementById("video") as HTMLVideoElement;
+  const audio = document.getElementById("recording") as HTMLAudioElement;
+  // const btnStop = document.getElementById("stop") as HTMLButtonElement;
+  // const btnPlay = document.getElementById("play") as HTMLButtonElement;
+  const btnRecord = document.getElementById("record") as HTMLButtonElement;
+  const inputRouting = document.getElementById("routing") as HTMLInputElement;
+
   const streamMic = await navigator.mediaDevices.getUserMedia({
     audio: {
-      echoCancellation: true,
+      echoCancellation: false,
       noiseSuppression: false,
       latency: { max: 10 / 1000 },
       sampleSize: { max: 64 },
+      channelCount: 2,
     },
   });
 
   const audioContext = new AudioContext();
   const recordingSource = audioContext.createMediaStreamSource(streamMic);
   const recordingDestination = audioContext.createMediaStreamDestination();
-  recordingSource.connect(recordingDestination);
+  const audioTrackVideo = audioContext.createMediaElementSource(video);
+  const recorder = new MediaRecorder(recordingDestination.stream);
+  audioTrackVideo.connect(audioContext.destination);
 
-  // const audioBuffer = await audioContext.decodeAudioData(ab);
-  let recorder: MediaRecorder;
   let isRecording = false;
-
-  const video = document.getElementById("video") as HTMLVideoElement;
-  const audio = document.getElementById("recording") as HTMLAudioElement;
-  // const btnStop = document.getElementById("stop") as HTMLButtonElement;
-  // const btnPlay = document.getElementById("play") as HTMLButtonElement;
-  const btnRecord = document.getElementById("record") as HTMLButtonElement;
-
-  video.addEventListener("canplay", async () => {
-    // btnPlay.addEventListener("click", () => {
-    //   video.play();
-    // });
-    const backing = audioContext.createMediaElementSource(video);
-
-    btnRecord.addEventListener("click", () => {
-      if (isRecording) {
-        isRecording = false;
-        btnRecord.innerHTML = "start recording";
-        video.pause();
-        recorder.stop();
-        return;
-      }
-      btnRecord.innerHTML = "stop recording";
-
-      // get audiostream from video
-      // connect to output so you can hear it
-      backing.connect(audioContext.destination);
-      // connect to recording destination
-      // backing.connect(recordingDestination);
-
-      video.play();
-
-      recorder = new MediaRecorder(recordingDestination.stream);
-      recorder.ondataavailable = async (event: { data: BlobPart }) => {
-        const blob = new Blob([event.data], { type: "audio/ogg; codecs=opus" });
-        const url = window.URL.createObjectURL(blob);
-        audio.src = url;
-        console.log("data available");
-        // const a = document.createElement("A") as HTMLAnchorElement;
-        // a.href = url;
-        // a.download = url;
-        // a.click();
-        // window.URL.revokeObjectURL(url);
-      };
-      recorder.start();
-      isRecording = true;
-    });
-
-    // btnStop.addEventListener("click", () => {
-    //   video.pause();
-    //   if (isRecording === true) {
-    //     // video.muted = false;
-    //     recorder.stop();
-    //     isRecording = false;
-    //   }
-    // });
-  });
+  let hardwareMixing = false;
 
   video.src = "./95BPM.mp4";
+
+  inputRouting.addEventListener("change", (e) => {
+    hardwareMixing = (e.target as HTMLInputElement).checked;
+  });
+
+  btnRecord.addEventListener("click", () => {
+    // route to speakers/headphones
+
+    if (isRecording) {
+      isRecording = false;
+      btnRecord.innerHTML = "start recording";
+      recorder.stop();
+      video.pause();
+      video.currentTime = 0;
+      if (hardwareMixing) {
+        try {
+          audioTrackVideo.disconnect(recordingDestination);
+        } catch (e) {
+          // console.log(e);
+        }
+      }
+      recordingSource.disconnect(recordingDestination);
+      return;
+    }
+
+    btnRecord.innerHTML = "stop recording";
+    console.log(hardwareMixing);
+    if (hardwareMixing === false) {
+      // add audio track of video to recording
+      audioTrackVideo.connect(recordingDestination);
+    }
+    recordingSource.connect(recordingDestination);
+
+    recorder.ondataavailable = async (event: { data: BlobPart }) => {
+      const blob = new Blob([event.data], { type: "audio/ogg; codecs=opus" });
+      const url = window.URL.createObjectURL(blob);
+      audio.src = url;
+      // const a = document.createElement("A") as HTMLAnchorElement;
+      // a.href = url;
+      // a.download = url;
+      // a.click();
+      // window.URL.revokeObjectURL(url);
+    };
+
+    video.play();
+    recorder.start();
+    isRecording = true;
+  });
+
+  // btnPlay.addEventListener("click", () => {
+  //   video.play();
+  // });
+
+  // btnStop.addEventListener("click", () => {
+  //   video.pause();
+  //   if (isRecording === true) {
+  //     // video.muted = false;
+  //     recorder.stop();
+  //     isRecording = false;
+  //   }
+  // });
+
+  // video.addEventListener("canplay", async () => {
+  // });
+
   // video.srcObject = s;
   // video.play();
   // video.muted = true;
